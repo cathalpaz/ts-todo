2907714efd12cfb438ac822cbb3bae79b1541e3d
Cathal <cathalpaz@gmail.com>
2023-10-04 18:12:29 -0400

    storage

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { v4 as uuidV4 } from "uuid"
 
 
+type Task = { id: string, title: string, completed: boolean, createdAt: Date }
+
 const list = document.querySelector<HTMLUListElement>('#list')
 const form = document.getElementById('#new-task-form') as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>('#new-task-title')
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
 
 form?.addEventListener('submit', e => {
     e.preventDefault()
@@ -11,11 +16,43 @@ form?.addEventListener('submit', e => {
     if (input?.value == '' || input?.value == null) return
 
 
-    const task = {
+    const newTask: Task = {
         id: uuidV4(),
         title: input.value,
         completed: false,
         createdAt: new Date()
     }
-    input.value
+    tasks.push(newTask)
+
+    addListItem(newTask)
+    input.value = ""
 })
+
+
+function addListItem(task: Task) {
+    const item = document.createElement('li')
+    const label = document.createElement('label')
+    const checkbox = document.createElement('input')
+    checkbox.addEventListener("change", () => {
+        task.completed = checkbox.checked
+        saveTasks()
+        console.log(tasks)
+    })
+    checkbox.type = 'checkbox'
+    checkbox.checked = task.completed
+    label.append(checkbox, task.title)
+    item.append(label)
+    list?.append(item)
+
+    return true
+}
+
+function saveTasks() {
+    localStorage.setItem('TASKS', JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+    const taskJSON = localStorage.getItem('TASKS')
+    if (taskJSON == null) return []
+    return JSON.parse(taskJSON)
+}
